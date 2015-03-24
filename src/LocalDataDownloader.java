63fa62558c6951d21beaf1fe7bbf95af24e34cba
Julien Guerinet <julien@guerinet.com>
2015-03-24 10:01:18 -0400

    Created the empty main file.

---
+++ src/LocalDataDownloader.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2015 Julien Guerinet. All rights reserved.
+ */
+
+/**
+ * Main file
+ * @author Julien Guerinet
+ * @version 1.0
+ * @since 1.0
+ */
+public class LocalDataDownloader {
+}
