ec5652152120ec6207d0a9f4e9d34d3eed63dc3d
Julien Guerinet <julien@guerinet.com>
2015-03-24 10:13:21 -0400

    Getting all of the parameters from the arguments.

--- src/LocalDataDownloader.java
+++ src/LocalDataDownloader.java
@@ -9,4 +9,37 @@
  * @since 1.0
  */
 public class LocalDataDownloader {
+    public static String url;
+    public static String fileName;
+    public static boolean basicAuthentication;
+    public static String username = "";
+    public static String password = "";
+    public static String directory = null;
+
+    public static void main(String[] args){
+        //Make sure there are the correct number of arguments
+        if(args.length < 3){
+            throw new IllegalArgumentException("You must have at least 3 arguments");
+        }
+
+        //Get the needed info from the args
+        url = args[0];
+        fileName = args[1];
+        basicAuthentication = Boolean.valueOf(args[2]);
+
+        //If we need basic auth, get the information
+        if(basicAuthentication){
+            username = args[3];
+            password = args[4];
+
+            //Check if there is a directory
+            if(args.length == 6){
+                directory = args[5];
+            }
+        }
+        //Check if there is a directory
+        else if(args.length == 4){
+            directory = args[3];
+        }
+    }
 }
